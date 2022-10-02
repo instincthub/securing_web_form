f42d3b0c4a69421a08dfecf8b1ad21558637683e
noaholatoye <noaholatoye@gmail.com>
2022-10-02 04:25:12 +0100

    Added success to JS

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
-const contactForm = document.getElementById('contact')
 
-// Not a number and length of number >= 10 
+const contactForm = document.getElementById('contact');
+const formInput = document.querySelectorAll('input');
+const formTextarea = document.querySelector('textarea');
+console.log(formInput)
+
+// Not a number and lengnth number of >= 10
 const notNumber = (e) =>{
-    if (Number(e) && e.length >= 10) {
-        return true
-    }
-    else{
-        return false
-    }
+  if(Number(e) && e.length >= 10){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
-// Check character length
+// Check character lenth
 const textLength = (e) =>{
-    if (e.length >= 3) {
-        return true
-    }
-    else{
-        return false
-    }
+  if(e.length >= 3){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 
-// Prevent default and handle error on submit.
+// Prevent default and handle errors.
 contactForm.addEventListener('submit', (e) =>{
-    e.preventDefault()
+  e.preventDefault()
 
-    const formInputs = contactForm.querySelectorAll('input')
-    const formTextarea= contactForm.querySelector('textarea')
+  formInput.forEach(element =>{
+    if(element.getAttribute('type') === 'tel'){
 
-    formInputs.forEach(element => {
-        if (element.getAttribute('type') === 'tel') {
-            if (!notNumber(element.value)) {
-                element.classList.add('err')
-            }
-        }
-        else if (!textLength(element.value)) {
-            element.classList.add('err')
-        }
-    });  
-
-    if (!textLength(formTextarea.value)) {
-        formTextarea.classList.add('err')
+      if(notNumber(element.value) === false){
+        element.classList.add('err')
+        element.classList.remove('success')
+      }
+      else if(notNumber(element.value)){
+        element.classList.remove('err')
+        element.classList.add('success')
+      }
     }
+    else{
+      if(textLength(element.value) === false){
+        element.classList.add('err')
+        element.classList.remove('success')
+      }
+      else{
+        element.classList.remove('err')
+        element.classList.add('success')
+      }
+    }
+
+    if(textLength(formTextarea.value) === false){
+      formTextarea.classList.add('err')
+      formTextarea.classList.remove('success')
+    }
+    else{
+      formTextarea.classList.remove('err')
+      formTextarea.classList.add('success')
+    }
+
+  })
+
+
 })
